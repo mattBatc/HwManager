b867e9a938646e8af755033ee50d43f4219bac05
Matt <mattbatc@gmail.com>
2018-04-06 11:46:48 -0400

    Updated gradle scripts, added some TODO's

--- app/src/main/java/com/codex/mattb/Activities/MainMenu.java
+++ app/src/main/java/com/codex/mattb/Activities/MainMenu.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainMenu extends AppCompatActivity {
 
+    //TODO Add UI
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
